dcde1a6f24fce5919c034f60582bac9fcd0f25f1
Sakhile-Msibi <thembinkosimsibi198@gmail.com>
2021-02-12 01:41:05 +0200

    brush up

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 	$DB_DSN = 'mysql:host=localhost;charset=UTF8';
 	$DB_NEW_DSN = 'mysql:host=localhost;dbname=camagru;charset=UTF8';
 	$DB_USER = 'root';
-	$DB_PASSWORD = 'Sakhile198';
+	$DB_PASSWORD = '';
 	$DB_NAME = 'camagru';
 
 ?>
